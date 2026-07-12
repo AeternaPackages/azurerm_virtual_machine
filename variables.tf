898f7a9d3cee53bf68658f3d4d4e0e5e5aebf056
Daniel Allen <dan@aeternagroup.net>
2026-07-12 22:00:28 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -49,6 +49,8 @@ Nested virtual_machine_extensions (azurerm_virtual_machine_extension):
         - automatic_upgrade_enabled
         - failure_suppression_enabled
         - protected_settings
+        - protected_settings_key_vault_id (alternative to protected_settings - read from Key Vault instead)
+        - protected_settings_key_vault_secret_name (alternative to protected_settings - read from Key Vault instead)
         - provision_after_extensions
         - settings
         - tags
@@ -89,6 +91,8 @@ Nested virtual_machine_run_commands (azurerm_virtual_machine_run_command):
         - error_blob_uri
         - output_blob_uri
         - run_as_password
+        - run_as_password_key_vault_id (alternative to run_as_password - read from Key Vault instead)
+        - run_as_password_key_vault_secret_name (alternative to run_as_password - read from Key Vault instead)
         - run_as_user
         - tags
         - error_blob_managed_identity (block)
@@ -203,17 +207,19 @@ EOT
       write_accelerator_enabled = optional(bool)   # Default: false
     })))
     virtual_machine_extensions = optional(map(object({
-      name                        = string
-      publisher                   = string
-      type                        = string
-      type_handler_version        = string
-      auto_upgrade_minor_version  = optional(bool)
-      automatic_upgrade_enabled   = optional(bool)
-      failure_suppression_enabled = optional(bool) # Default: false
-      protected_settings          = optional(string)
-      provision_after_extensions  = optional(list(string))
-      settings                    = optional(string)
-      tags                        = optional(map(string))
+      name                                     = string
+      publisher                                = string
+      type                                     = string
+      type_handler_version                     = string
+      auto_upgrade_minor_version               = optional(bool)
+      automatic_upgrade_enabled                = optional(bool)
+      failure_suppression_enabled              = optional(bool) # Default: false
+      protected_settings                       = optional(string)
+      protected_settings_key_vault_id          = optional(string)
+      protected_settings_key_vault_secret_name = optional(string)
+      provision_after_extensions               = optional(list(string))
+      settings                                 = optional(string)
+      tags                                     = optional(map(string))
       protected_settings_from_key_vault = optional(object({
         secret_url      = string
         source_vault_id = string
@@ -253,13 +259,15 @@ EOT
       })))
     })))
     virtual_machine_run_commands = optional(map(object({
-      location        = string
-      name            = string
-      error_blob_uri  = optional(string)
-      output_blob_uri = optional(string)
-      run_as_password = optional(string)
-      run_as_user     = optional(string)
-      tags            = optional(map(string))
+      location                              = string
+      name                                  = string
+      error_blob_uri                        = optional(string)
+      output_blob_uri                       = optional(string)
+      run_as_password                       = optional(string)
+      run_as_password_key_vault_id          = optional(string)
+      run_as_password_key_vault_secret_name = optional(string)
+      run_as_user                           = optional(string)
+      tags                                  = optional(map(string))
       source = object({
         command_id = optional(string)
         script     = optional(string)
